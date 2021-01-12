b1d33f1eb99a362f18439cfed71b922fd9ce0c7a
yigit polat <yigit@u.nus.edu>
2021-01-12 10:26:09 +0800

    ball impl

--- include/misc.hpp
+++ include/misc.hpp
@@ -13,9 +13,27 @@ namespace misc
     // Chapter 10
     void print_employee_name( const Employee &employee);
 
+    // Chapter 11
+    class Ball
+    {
+        std::string m_color{"black"};
+        double m_radius{10.0};
+
+    public:
+        Ball() = default;
+        Ball(const std::string &color, double radius);
+        Ball(const std::string &color);
+        Ball(double radius);
+
+        void print() const;
+    };
+
     namespace demos
     {
         // Chapter 10
         void demo_print_employee_name();
+        
+        // Chapter 11
+        void demo_ball();
     }
 }
--- src/misc.cpp
+++ src/misc.cpp
@@ -3,7 +3,30 @@
 //std headers
 #include <iostream> //std::cout, std::endl
 
-void misc::print_employee_name( const Employee &employee)
+void misc::print_employee_name(const Employee &employee)
 {
     std::cout << employee.name;
+}
+
+misc::Ball::Ball(const std::string &color, double radius) :
+    m_color{color}, m_radius{radius}
+{
+
+}
+
+misc::Ball::Ball(const std::string &color) :
+    m_color{color}
+{
+
+}
+
+misc::Ball::Ball(double radius) :
+    m_radius{radius}
+{
+
+}
+
+void misc::Ball::print() const
+{
+    std::cout << "color: " << m_color << ", radius: " << m_radius << std::endl;
 }
